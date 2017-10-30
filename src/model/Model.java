fc6b0c884d477d330e1edfcb646637a9586c4010
Unknown <zenghoulim@outlook.com>
2017-10-30 18:50:59 +0800

    Model: Update API

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,9 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+
+import com.ib.client.Contract;
+
 /**
  * The API of the model component
  */
 public interface Model {
-    /** Parses a CSV file containing the list of ticker symbols */
-    void getListOfSymbols();
+    /** Returns a UniqueContractList object */
+    UniqueContractList getUniqueContractList();
+
+    /** Returns a list of iterable Contracts from a UniqueContractList object */
+    ArrayList<Contract> getViewOnlyContractList();
+
+    /** Sets up a model with a {@see listOfSymbols} and {@see UniqueContractList} */
+    void initializeModel();
+
+    /** Returns an String arraylist for the {@see Parser} to populate with symbols */
+    ArrayList<String> getListOfSymbolsArray();
 }
