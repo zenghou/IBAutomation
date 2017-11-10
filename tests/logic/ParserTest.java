d9f355a2b0ac3cc60a10af44b563710c201a16b9
Unknown <zenghoulim@outlook.com>
2017-11-10 23:50:05 +0800

    Add ParserTest

---
+++ tests/logic/ParserTest.java
@@ -0,0 +1,14 @@
+package logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class ParserTest {
+    @Test
+    public void parseCsvFile_returnsCorrectOutput() {
+        // TODO: add test
+    }
+
+}
