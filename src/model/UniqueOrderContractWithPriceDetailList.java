c2b4e179ebf0f14fcd18dda7d45d5f71a5967c48
Unknown <zenghoulim@outlook.com>
2017-11-10 23:41:56 +0800

    Refactor UniqueOrderContractWithPriceDetailList

---
+++ src/model/UniqueOrderContractWithPriceDetailList.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Stores a list of unique ContractWithPriceDetail which should be submitted for limit buy order
+ */
+public class UniqueOrderContractWithPriceDetailList extends UniqueContractWithPriceDetailList {
+    public UniqueOrderContractWithPriceDetailList() {
+        super();
+    }
+}
