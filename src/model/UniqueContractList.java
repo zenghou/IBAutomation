a312dced2003adb01fce0e639dc58d552ff7abf1
Unknown <zenghoulim@outlook.com>
2017-11-28 16:20:55 +0800

    UniqueContractList: refactor attribute name and add default size

--- src/model/UniqueContractList.java
+++ src/model/UniqueContractList.java
@@ -3,33 +3,45 @@ package model;
 import java.util.ArrayList;
 
 import model.exceptions.DuplicateContractException;
+import model.exceptions.FullContractListException;
 
 /**
  * Stores a list of unique ContractWithPriceDetail which are to be monitored with live stream of price data
  */
 public class UniqueContractList {
-    private final ArrayList <ContractWithPriceDetail> contractWithPriceDetailArrayListArrayList;
+    private final int DEFAULT_ARRAY_SIZE = 1000;
+    // default array size will be 1000
+    private int arraySize = DEFAULT_ARRAY_SIZE;
 
+    private final ArrayList <ContractWithPriceDetail> contractWithPriceDetailArrayList;
+
+    // initializes a contractWithPriceDetailArrayList that can hold 1000 contracts
     public UniqueContractList() {
-        contractWithPriceDetailArrayListArrayList = new ArrayList<>();
+        contractWithPriceDetailArrayList = new ArrayList<>(arraySize);
     }
 
     public UniqueContractList(int maxNumberOfContracts) {
-        contractWithPriceDetailArrayListArrayList = new ArrayList<>(maxNumberOfContracts);
+        // replace default array size
+        arraySize = maxNumberOfContracts;
+        contractWithPriceDetailArrayList = new ArrayList<>(maxNumberOfContracts);
     }
 
-    public void addContract(ContractWithPriceDetail contract) throws DuplicateContractException{
-        if (contractWithPriceDetailArrayListArrayList.contains(contract)) {
+    public void addContract(ContractWithPriceDetail contract) throws FullContractListException,
+            DuplicateContractException {
+        if (contractWithPriceDetailArrayList.size() == arraySize) {
+            throw new FullContractListException();
+        }
+        if (contractWithPriceDetailArrayList.contains(contract)) {
             throw new DuplicateContractException();
         }
-        contractWithPriceDetailArrayListArrayList.add(contract);
+        contractWithPriceDetailArrayList.add(contract);
     }
 
     public void removeContract(ContractWithPriceDetail contract) {
-        contractWithPriceDetailArrayListArrayList.remove(contract);
+        contractWithPriceDetailArrayList.remove(contract);
     }
 
     public ArrayList<ContractWithPriceDetail> getContractArrayWithPriceDetailList() {
-        return contractWithPriceDetailArrayListArrayList;
+        return contractWithPriceDetailArrayList;
     }
 }
