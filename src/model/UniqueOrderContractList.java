bf018c205881639d6fcb2789f44af642204fb619
Unknown <zenghoulim@outlook.com>
2017-11-28 18:04:16 +0800

    Override UniqueContractList method to ask Logic to send order

--- src/model/UniqueOrderContractList.java
+++ src/model/UniqueOrderContractList.java
@@ -1,9 +1,15 @@
 package model;
 
+import logic.Logic;
+import model.exceptions.DuplicateContractException;
+import model.exceptions.FullContractListException;
+
 /**
  * Stores a list of unique ContractWithPriceDetails which should be submitted for limit buy order
  */
 public class UniqueOrderContractList extends UniqueContractList {
+    private Logic logic = null;
+
     // use superclass's default constructor
     public UniqueOrderContractList() {
         super();
@@ -11,5 +17,28 @@ public class UniqueOrderContractList extends UniqueContractList {
 
     public UniqueOrderContractList(int maxNumberOfContracts) {
         super(maxNumberOfContracts);
+    }
+
+    public void setLogic(Logic logic) {
+        this.logic = logic;
+    }
+
+    @Override
+    public void addContract(ContractWithPriceDetail contract) throws FullContractListException,
+            DuplicateContractException {
+        super.addContract(contract);
+        newContractAddedNotifyLogicToSendOrder(contract);
+    }
+
+    /**
+     * Asks Logic to send order when ContractWithPriceDetail is added to UniqueContractOrderList
+     * Could consider using Google's event bus/Observer & Subscriber model to reduce coupling
+     * @param contract
+     */
+    private void newContractAddedNotifyLogicToSendOrder (ContractWithPriceDetail contract) {
+        // logic must be set up
+        assert(logic != null);
+
+        logic.placeLimitBuyOrder(contract);
     }
 }
