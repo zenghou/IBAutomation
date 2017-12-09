ef898c294436fe3d9b04ba960b09beb267f6512f
Unknown <zenghoulim@outlook.com>
2017-12-09 21:25:37 +0800

    ContractBuilder.java: update constructor for ContractWithPriceDetail

--- src/logic/ContractBuilder.java
+++ src/logic/ContractBuilder.java
@@ -6,18 +6,23 @@ import com.ib.client.Contract;
 import model.ContractWithPriceDetail;
 
 public class ContractBuilder {
+    public static final double MINIMUM_PERCENTAGE_DECREASE = 13.00;
+
     /**
      * Creates a {@see ContractWithPriceDetail} object with a stock symbol
-     * @param stockSymbol
+     * @param ticker
      * @return a ContractWithPriceDetail object
      */
-    public static ContractWithPriceDetail buildContractWithPriceDetail(String stockSymbol) {
-        ContractWithPriceDetail contract = new ContractWithPriceDetail();
-        contract.symbol(stockSymbol);
+    public static ContractWithPriceDetail buildContractWithPriceDetail(String ticker, double openingPrice) {
+        ContractWithPriceDetail contract = new ContractWithPriceDetail(openingPrice,
+                MINIMUM_PERCENTAGE_DECREASE);
+
+        contract.symbol(ticker);
         contract.secType("STK");
         contract.currency("USD");
         contract.exchange("SMART");
         contract.primaryExch("ISLAND");
+
         return contract;
     }
 }
