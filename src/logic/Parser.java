39708d469359bea92cfcc367e803b9ea290f6a33
Unknown <zenghoulim@outlook.com>
2017-12-19 23:52:43 +0800

    Parser: add method to check CSV file periodically

--- src/logic/Parser.java
+++ src/logic/Parser.java
@@ -5,18 +5,24 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.TimerTask;
 
+import model.ContractBuilder;
 import model.Model;
 
 /**
- * Parses a CSV file of ticker symbols and add
+ * Parses a CSV file of ticker symbols and adds Contracts to the {@see UniqueContractList} periodically (estimated to be
+ * every 30 seconds)
  */
-public class Parser {
+public class Parser extends TimerTask {
     private Model model;
     private Scanner scanner;
+    private String filepath;
 
     public Parser(String filepath, Model model) {
+        this.filepath = filepath;
         setModel(model);
+
         try {
             scanner = new Scanner(new File(filepath));
         } catch (FileNotFoundException fnfe) {
@@ -31,6 +37,7 @@ public class Parser {
 
     /**
      * Reads CSV file from filepath with Scanner and adds each symbol into Model's {@code tickerPriceHashMap}
+     * for the first time
      */
     public void readDataUpdateModel() {
         while (scanner.hasNextLine()) {
@@ -42,6 +49,42 @@ public class Parser {
 
             HashMap<String, Double> tickerPriceHashMap = model.getTickerPriceHashMap();
             tickerPriceHashMap.put(ticker, price);
+        }
+    }
+
+    /**
+     * Continually opens and checks a CSV file for updated ticker symbols and adds them to the model's
+     * {@see UniqueContractList}
+     * @throws FileNotFoundException
+     */
+    private void checkForUpdates() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filepath));
+
+        while (scanner.hasNextLine()) {
+            String eachLine = scanner.nextLine();
+            String[] tickerPriceList = eachLine.split(" ");
+            String ticker = tickerPriceList[0];
+            Double price = Double.parseDouble(tickerPriceList[1]);
+
+            HashMap<String, Double> tickerPriceHashMap = model.getTickerPriceHashMap();
+
+            // only add to tickerPriceHashMap if ticker is inside
+            if (!tickerPriceHashMap.containsKey(ticker)) {
+                tickerPriceHashMap.put(ticker, price);
+
+                model.updateUniqueContractList(ContractBuilder.buildContractWithPriceDetail(ticker, price));
+            }
+
+        }
+    }
+
+    // To be called by Timer object
+    @Override
+    public void run() {
+        try {
+            checkForUpdates();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
