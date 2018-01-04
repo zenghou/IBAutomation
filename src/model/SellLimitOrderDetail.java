ad3c5ef6180fa308324051abd9fa2a013eb2b311
Unknown <zenghoulim@outlook.com>
2018-01-04 20:23:41 +0800

    add SellLimitOrderDetail: to keep track of order id and symbols

---
+++ src/model/SellLimitOrderDetail.java
@@ -0,0 +1,80 @@
+package model;
+
+/**
+ * Stores the details (orderId, shares filled, shares remaining) for a particular sell order when
+ * {@see EWrapperImplementation#orderStatus()} is called. Object can be uniqued identified by the {@see orderId} assigned.
+ */
+public class SellLimitOrderDetail {
+    private int orderId;
+    private double filled;
+    private double remaining;
+    private boolean isFullyFilled;
+    private boolean hasSymbol;
+    private String symbol;
+
+    public SellLimitOrderDetail(int orderId, double filled, double remaining) {
+        this.orderId = orderId;
+        this.filled = filled;
+        this.remaining = remaining;
+        hasSymbol = false;
+        symbol = null;
+
+        if (remaining == 0.00) {
+            isFullyFilled = true;
+        } else {
+            isFullyFilled = false;
+        }
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public double getFilled() {
+        return filled;
+    }
+
+    public double getRemaining() {
+        return remaining;
+    }
+
+    /** Takes in an orderId and checks if the details correspond by verifying this object's orderId */
+    public boolean matchesOrderId(int orderId) {
+        return (this.orderId == orderId);
+    }
+
+    public void fillMoreShares(double additionalSharesFilled) {
+        filled += additionalSharesFilled;
+        reduceRemainingShares(additionalSharesFilled);
+        if (remaining == 0.00) {
+            isFullyFilled = true;
+        } else {
+            isFullyFilled = false;
+        }
+    }
+
+    private void reduceRemainingShares(double additionalSharesFilled) {
+        remaining -= additionalSharesFilled;
+    }
+
+    public boolean isFullyFilled() {
+        return isFullyFilled;
+    }
+
+    public void setSymbol(String symbol) throws Exception {
+        if (this.symbol.equals(null)) {
+            this.symbol = symbol;
+            this.hasSymbol = true;
+        } else {
+            throw new Exception("SellLimitOrderDetail already has a symbol: " + symbol);
+        }
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public boolean hasToBeClosedByMOC() {
+        return hasSymbol && (getRemaining() > 0.00) && (!isFullyFilled);
+    }
+}
