c09ad55f6d45cba195afb0574b9e2dcfe5525ead
Unknown <zenghoulim@outlook.com>
2018-01-04 20:29:13 +0800

    ScheduledCancelUnfilledOrders: add scheduled task to cancel orders

---
+++ src/logic/ScheduledCancelUnfilledOrders.java
@@ -0,0 +1,46 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.TimerTask;
+import java.util.logging.Logger;
+
+import model.Model;
+
+/** A scheduled task that cancels all unfilled sell limit orders at a set time */
+public class ScheduledCancelUnfilledOrders extends TimerTask {
+    private Logic logic;
+    private Model model;
+
+    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    public ScheduledCancelUnfilledOrders(Logic logic, Model model) {
+        this.logic = logic;
+        this.model = model;
+    }
+
+    @Override
+    public void run() {
+        ArrayList<Integer> orderIdsForCancellation = model.getOrderIdsForUnfilledSellLimitOrders();
+        cancelUnfilledSellLimitOrders(orderIdsForCancellation);
+    }
+
+    private void cancelUnfilledSellLimitOrders(ArrayList<Integer> orderIds) {
+        for (Integer orderId : orderIds) {
+            LOGGER.info("***************************[ Attempting to cancel sell limit order id: " + orderId + " ]***" +
+                    "***************************");
+            logic.cancelOrder(orderId);
+
+            LOGGER.info("===========================[ Attempting to remove order id: " + orderId + " ===" +
+                    "===========================");
+
+            try {
+                model.removeSellLimitOrderId(orderId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            LOGGER.info("===========================[ Order id: " + orderId + " removed from sellLimitOrderList!" +
+                    " ]==============================");
+        }
+    }
+}
