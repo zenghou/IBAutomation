8ff21ad44c87c80c4c6317222c833706f9fcddf5
Unknown <zenghoulim@outlook.com>
2018-01-04 20:27:25 +0800

    DateBuilder: new class that returns a cancellation and closing time

---
+++ src/logic/DateBuilder.java
@@ -0,0 +1,40 @@
+package logic;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/** Returns a Date object at a fixed time for each day */
+public class DateBuilder {
+
+    private static final int HOUR = 04;
+    private static final int MINUTE_CANCELLATION = 40;
+    private static final int MINUTE_CLOSING = 41;
+    private static final int SECOND = 0;
+    private static final int MILLISECOND = 0;
+    private static final int ONE_DAY = 1;
+
+
+    public static Date getCancellationTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, HOUR);
+        calendar.set(Calendar.MINUTE, MINUTE_CANCELLATION);
+        calendar.set(Calendar.SECOND, SECOND);
+        calendar.set(Calendar.MILLISECOND, MILLISECOND);
+
+        calendar.add(Calendar.DATE, 1);
+
+        return calendar.getTime();
+    }
+
+    public static Date getMarketOnCloseTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, HOUR);
+        calendar.set(Calendar.MINUTE, MINUTE_CLOSING);
+        calendar.set(Calendar.SECOND, SECOND);
+        calendar.set(Calendar.MILLISECOND, MILLISECOND);
+
+        calendar.add(Calendar.DATE, ONE_DAY);
+
+        return calendar.getTime();
+    }
+}
